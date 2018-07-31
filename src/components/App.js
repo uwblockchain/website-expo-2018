835376d5e499998a7f4381bf47aedd32d2d915ba
Brady Pascoe <bpas247@hotmail.com>
2018-07-31 00:30:59 -0700

    Added more components to navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import '../css/App.css';
-import NavBar from './NavBar'
+import Navigation from './Navigation'
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <NavBar />
+        <Navigation />
       </div>
     );
   }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,16 +1,68 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography'
 
-export default class Navigation extends Component {
-    render() {
-        return(
-            <div className="navigation">
-                <a href="#">Home</a>
-                <a href="#">Contact</a>
-                <a href="#">Speakers</a>
-                <a href="#">Workshops</a>
-                <a href="#">Sponsors</a>
-                <a href="#">Booth</a>
-            </div>
-        )
+function TabContainer(props) {
+  return (
+    <Typography component="div" style={{ padding: 8 * 3 }}>
+      {props.children}
+    </Typography>
+    );
+  }  
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  flex: {
+    flexGrow: 1
     }
+  };
+  
+
+class Navigation extends Component {
+  state = {
+    value: 0,
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { value } = this.state;
+
+    return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="title" color="inherit">
+              UW Blockchain Expo
+            </Typography>
+            <Button variant="outlined" color="inherit">
+              Get Tickets
+            </Button>
+            <Tabs value={value} onChange={this.handleChange}>
+              <Tab label="Speakers" />
+              <Tab label="Workshops" />
+              <Tab label="Sponsors" />
+              <Tab label="Contact" />
+            </Tabs>
+          </Toolbar>
+        </AppBar>
+        {value === 0 && <TabContainer>Item One</TabContainer>}
+        {value === 1 && <TabContainer>Item Two</TabContainer>}
+        {value === 2 && <TabContainer>Item Three</TabContainer>}
+        {value === 3 && <TabContainer>Item Four</TabContainer>}
+      </div>
+    );
+  }
 }
+
+export default withStyles(styles)(Navigation);
