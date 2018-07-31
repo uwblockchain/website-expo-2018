a61a18ec7103db6c139d9739853c137d995b22c8
Brady Pascoe <bpas247@hotmail.com>
2018-07-30 23:21:02 -0700

    Material UI demo

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import Header from './Header';
 import '../css/App.css';
+import NavBar from './NavBar'
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header/>
+        <NavBar />
       </div>
     );
   }
