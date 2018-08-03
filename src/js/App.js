cc8ee24401a46ce18ff179c2ee9804bb708a1df2
Brady Pascoe <bpas247@hotmail.com>
2018-08-03 15:08:05 -0700

    Preliminary draft

--- src/js/App.js
+++ src/js/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import '../css/App.css';
+import Speakers from './Speakers';
 
 class App extends Component {
   render() {
     return (
-      <div className="App"></div>
+      <div className="App">
+        <Speakers />
+      </div>
     );
   }
 }
---
+++ src/js/Speakers.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Speakers = () => (
+  <h1>Speakers</h1>
+
+);
+
+export default Speakers;
