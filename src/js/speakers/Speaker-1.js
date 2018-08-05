890e73d4c16cfe5423a023fbeaf0eca70e9d3a3b
Brady Pascoe <bpas247@hotmail.com>
2018-08-05 14:55:06 -0700

    Added indentation

--- src/js/speakers/Speaker-1.js
+++ src/js/speakers/Speaker-1.js
@@ -18,24 +18,24 @@ const Speaker1 = () => (
         </h2>
       </div>
       <div className="description">
-      Ahmad Alkabra is the Global Director of Product at Appsolutely, a company that is 
-      disrupting and reimagining the customer loyalty space and ecosystem with LoyalCoin 
-      - the next-generation customer loyalty solution on the blockchain. At Appsolutely,
-      Ahmad guides LoyalCoin's product strategy and spearheads the delivery of apps,
-      services, and solutions related to the LoyalCoin ecosystem.
-      <br />
-      <br />
-      Prior to joining Appsolutely, Ahmad had a stellar run at T-Mobile where he co-
-      started, built and launched T-Mobile Tuesdays – an innovative customer
-      engagement, and industry first customer loyalty app and platform that has been an
-      instant success since its launch in 2016.
-      <br />
-      <br />
-      He sees blockchain as the next evolution in the loyalty space and LoyalCoin as a
-      universal and interoperable solution that utilizes the power of cryptocurrency and
-      blockchain technology to incentivize customers into becoming more engaged and
-      satisfied with loyalty/rewards programs, driving customer acquisition and retention
-      for merchants and brands.
+        Ahmad Alkabra is the Global Director of Product at Appsolutely, a company that is 
+        disrupting and reimagining the customer loyalty space and ecosystem with LoyalCoin 
+        - the next-generation customer loyalty solution on the blockchain. At Appsolutely,
+        Ahmad guides LoyalCoin's product strategy and spearheads the delivery of apps,
+        services, and solutions related to the LoyalCoin ecosystem.
+        <br />
+        <br />
+        Prior to joining Appsolutely, Ahmad had a stellar run at T-Mobile where he co-
+        started, built and launched T-Mobile Tuesdays – an innovative customer
+        engagement, and industry first customer loyalty app and platform that has been an
+        instant success since its launch in 2016.
+        <br />
+        <br />
+        He sees blockchain as the next evolution in the loyalty space and LoyalCoin as a
+        universal and interoperable solution that utilizes the power of cryptocurrency and
+        blockchain technology to incentivize customers into becoming more engaged and
+        satisfied with loyalty/rewards programs, driving customer acquisition and retention
+        for merchants and brands.
       </div>
     </div>
   </div>
