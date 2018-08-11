400157b9b36cdfd89bae89e2ed81067ca07dc566
Brady Pascoe <bpas247@hotmail.com>
2018-08-11 11:24:35 -0700

    Added stylesheet

--- src/js/App.js
+++ src/js/App.js
@@ -6,6 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossOrigin="anonymous" />
         <Speakers />
       </div>
     );
