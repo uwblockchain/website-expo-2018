d917c0c53297fb405e4cb8d52768361ee4999e25
Brady Pascoe <bpas247@hotmail.com>
2018-08-11 17:34:48 -0700

    Migrated to react-bootstrap

--- src/js/Navigation.js
+++ src/js/Navigation.js
@@ -1,85 +1,36 @@
-import React, { Component } from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import PropTypes from 'prop-types';
+import React from 'react';
+import { Navbar, Nav, NavItem, Tabs, Tab, Button } from 'react-bootstrap';
 
-function TabContainer(props) {
-  return (
-    <Typography component="div" style={{ padding: 8 }}>
-      {props.children}
-    </Typography>
-    );
-  }  
+const Navigation = () => (
+  <Navbar>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <a>UW Blockchain Expo</a>
+      </Navbar.Brand>
+    </Navbar.Header>
+    <Navbar.Text>
+      Oct 13
+      <br />
+      2018  
+    </Navbar.Text>
+    <Nav pullRight>
+      <NavItem>
+        <Button>Get Tickets</Button>
+      </NavItem>
+      <NavItem eventKey={1} href="#">
+        Speakers
+      </NavItem>
+      <NavItem eventKey={2} href="#">
+        Workshops
+      </NavItem>
+      <NavItem eventKey={3} href="#">
+        Sponsors
+      </NavItem>
+      <NavItem eventKey={4} href="#">
+        Contact
+      </NavItem>
+    </Nav>
+  </Navbar>
+);
 
-const styles = {
-  root: {
-    flexGrow: 1,
-  },
-  flex: {
-    flexGrow: 1
-    },
-  menu: {
-    marginLeft: '40%'
-  }
-  };
-  
-
-class Navigation extends Component {
-  constructor() {
-    super();
-    this.handleChange = this.handleChange.bind(this);
-  }
-  state = {
-    value: 0,
-  };
-
-  handleChange = (event, value) => {
-    this.setState({ value });
-  };
-
-  render() {
-    const { classes } = this.props;
-    const { value } = this.state;
-
-    return (
-      <div className={classes.root}>
-        <AppBar position="static">
-          <Toolbar variant="regular">
-            <Typography variant="title" color="inherit">
-              UW Blockchain Expo
-            </Typography>
-            <Typography variant="subheading" color="inherit">
-              Oct 13
-              <br />
-              2018
-            </Typography>
-            <Button className={classes.menu} variant="outlined" color="inherit" href="https://www.eventbrite.com/e/uw-blockchain-expo-tickets-47000447604">
-              Get Tickets
-            </Button>
-            <Tabs value={value} onChange={this.handleChange}>
-              <Tab className="tab" label="Speakers" />
-              <Tab className="tab" label="Workshops" />
-              <Tab className="tab" label="Sponsors" />
-              <Tab className="tab" label="Contact" />
-            </Tabs>
-          </Toolbar>
-        </AppBar>
-        {value === 0 && <TabContainer>Speaker Information</TabContainer>}
-        {value === 1 && <TabContainer>Workshop Information</TabContainer>}
-        {value === 2 && <TabContainer>Sponsor Information</TabContainer>}
-        {value === 3 && <TabContainer>Contact Information</TabContainer>}
-      </div>
-    );
-  }
-}
-
-Navigation.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(Navigation);
+export default Navigation;
