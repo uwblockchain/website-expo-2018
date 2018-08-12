097db1848a74830da3607c16ef32acb0ca416a9c
Brady Pascoe <bpas247@hotmail.com>
2018-08-11 17:38:31 -0700

    Migrated to react-bootstrap

--- src/js/App.js
+++ src/js/App.js
@@ -4,7 +4,9 @@ import '../css/App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App"></div>
+      <div className="App">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossOrigin="anonymous" />
+      </div>
     );
   }
 }
