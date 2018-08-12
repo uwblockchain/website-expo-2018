305524bb1649abf93c3af37d71015cfb8e99a467
Brady Pascoe <bpas247@hotmail.com>
2018-08-12 16:08:38 -0700

    Added header functionality
    will now switch the view of the content on the page

--- src/js/App.js
+++ src/js/App.js
@@ -8,6 +8,7 @@ export default class App extends Component {
       <div className="App">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossOrigin="anonymous" />
         <Navigation />
+        <div id="content"></div>
       </div>
     );
   }
--- src/js/Navigation.js
+++ src/js/Navigation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { Navbar, Nav, NavItem, Button } from 'react-bootstrap';
 
 const navigation = {
@@ -20,6 +21,29 @@ const button = {
   color: 'white',
 };
 
+function handleSelect(selectedKey) {
+  var renderContent; //Used as the out parameter
+  switch(selectedKey) {
+    case 1:
+      renderContent = <div>Speaker Content</div>;
+      break;
+    case 2:
+      renderContent = <div>Workshop Content</div>;
+      break;
+    case 3:
+      renderContent = <div>Sponsor Content</div>;
+      break;
+    case 4:
+      renderContent = <div>Contact Content</div>;
+      break;
+    default:
+      renderContent = <div>No Content</div>
+      break;
+  }
+  //Render the content to the DOM
+  ReactDOM.render(renderContent, document.getElementById('content'));
+}
+
 const Navigation = () => (
   <Navbar style={navigation} inverse collapseOnSelect>
     <Navbar.Header>
@@ -38,26 +62,28 @@ const Navigation = () => (
         <NavItem href="https://www.eventbrite.com/e/uw-blockchain-expo-tickets-47000447604">
           <Button style={button}>Get Tickets</Button>
         </NavItem>
-        <NavItem className="tab" eventKey={1} href="#">
-          <div style={text}>
-            Speakers
-          </div>
-        </NavItem>
-        <NavItem className="tab" eventKey={2} href="#">
-          <div style={text}>
-            Workshops
-          </div>
-        </NavItem>
-        <NavItem className="tab" eventKey={3} href="#">
-          <div style={text}>
-            Sponsors
-          </div>
-        </NavItem>
-        <NavItem className="tab" eventKey={4} href="#">
-          <div style={text}>
-            Contact
-          </div>
-        </NavItem>
+        <Nav onSelect={handleSelect}>
+          <NavItem className="tab" eventKey={1} href="#">
+            <div style={text}>
+              Speakers
+            </div>
+          </NavItem>
+          <NavItem className="tab" eventKey={2} href="#">
+            <div style={text}>
+              Workshops
+            </div>
+          </NavItem>
+          <NavItem className="tab" eventKey={3} href="#">
+            <div style={text}>
+              Sponsors
+            </div>
+          </NavItem>
+          <NavItem className="tab" eventKey={4} href="#">
+            <div style={text}>
+              Contact
+            </div>
+          </NavItem>
+        </Nav>
       </Nav>
     </Navbar.Collapse>
   </Navbar>
