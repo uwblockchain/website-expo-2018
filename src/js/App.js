949e012e86985d61acfccf224ef36dc0b0ac0da0
Brady Pascoe <bpas247@hotmail.com>
2018-08-12 18:12:56 -0700

    Updated font and modulated head

--- src/js/App.js
+++ src/js/App.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react';
 import '../css/App.css';
-import Navigation from './Navigation'
+import React, { Component } from 'react';
+import Navigation from './Navigation';
+import Head from './Head';
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossOrigin="anonymous" />
+        <Head />
         <Navigation />
-        <div id="content"></div>
+        <div id="content" />
       </div>
     );
   }
---
+++ src/js/Head.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+
+const Head = () => (
+  <Helmet>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossOrigin="anonymous" />
+  </Helmet>
+);
+
+export default Head;
