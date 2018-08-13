9e06e2a190287ffde3828f0d1f96c65ab203e0b4
Brady Pascoe <bpas247@hotmail.com>
2018-08-12 20:02:27 -0700

    Added more navigation tests

--- src/js/Navigation.test.js
+++ src/js/Navigation.test.js
@@ -2,11 +2,26 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Navigation from './Navigation';
 
-it('renders without crashing', () => {
-  shallow(<Navigation />);
+var wrapper;
+
+beforeEach(() => {
+  wrapper = shallow(<Navigation />);
 });
 
+it('renders without crashing', () => {});
+
 it('renders 4 tabs', () => {
-  const wrapper = shallow(<Navigation />);
   expect(wrapper.find('.tab').length === 4).toBe(true);
+});
+
+it('renders the button', () => {
+  expect(wrapper.find('.button').length == 1).toBe(true);
+});
+
+it('renders the title', () => {
+  expect(wrapper.find('.title').length == 1).toBe(true);
+});
+
+it('renders the date', () => {
+  expect(wrapper.find('.date').length == 1).toBe(true);
 });
