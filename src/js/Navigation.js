c5b8f30fbd9468978719acb5d55ddd974524f6c1
Brady Pascoe <bpas247@hotmail.com>
2018-08-18 14:11:32 -0700

    Updated navigation to use NavTabs

--- src/js/Navigation.js
+++ src/js/Navigation.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { Navbar, Nav, NavItem, Button } from 'react-bootstrap';
+import NavTabs from './NavTabs';
 
+// Styles 
 const navigation = {
   background: 'inherit',
   border: 'none',
@@ -13,10 +14,6 @@ const title = {
   fontWeight: 'bold',
   letterSpacing: '2px',
 }
-
-const text = {
-  color: 'white',
-};
 
 const date = {
   color: 'white',
@@ -31,29 +28,6 @@ const button = {
   background: 'inherit', 
   color: 'white',
 };
-
-function handleSelect(selectedKey) {
-  var renderContent; //Used as the out parameter
-  switch(selectedKey) {
-    case 1:
-      renderContent = <div>Speaker Content</div>;
-      break;
-    case 2:
-      renderContent = <div>Workshop Content</div>;
-      break;
-    case 3:
-      renderContent = <div>Sponsor Content</div>;
-      break;
-    case 4:
-      renderContent = <div>Contact Content</div>;
-      break;
-    default:
-      renderContent = <div>No Content</div>
-      break;
-  }
-  //Render the content to the DOM
-  ReactDOM.render(renderContent, document.getElementById('content'));
-}
 
 const Navigation = () => (
   <Navbar style={navigation} inverse collapseOnSelect>
@@ -73,29 +47,8 @@ const Navigation = () => (
         <NavItem className="button" href="https://www.eventbrite.com/e/uw-blockchain-expo-tickets-47000447604">
           <Button style={button}>Get Tickets</Button>
         </NavItem>
-        <Nav onSelect={handleSelect}>
-          <NavItem className="tab" eventKey={1} href="#">
-            <div style={text}>
-              Speakers
-            </div>
-          </NavItem>
-          <NavItem className="tab" eventKey={2} href="#">
-            <div style={text}>
-              Workshops
-            </div>
-          </NavItem>
-          <NavItem className="tab" eventKey={3} href="#">
-            <div style={text}>
-              Sponsors
-            </div>
-          </NavItem>
-          <NavItem className="tab" eventKey={4} href="#">
-            <div style={text}>
-              Contact
-            </div>
-          </NavItem>
-        </Nav>
-      </Nav>
+        <NavTabs />
+      </Nav> 
     </Navbar.Collapse>
   </Navbar>
 );
