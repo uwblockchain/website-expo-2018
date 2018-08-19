e8ca6e9ff72ac6af945a11ed22024c6f1115994b
Brady Pascoe <bpas247@hotmail.com>
2018-08-18 18:54:16 -0700

    Migrated all head elements to the Head component

--- src/js/Head.js
+++ src/js/Head.js
@@ -3,8 +3,11 @@ import Helmet from 'react-helmet';
 
 const Head = () => (
   <Helmet>
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet" />
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossOrigin="anonymous" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
+    <title>UW Blockchain Expo</title>
   </Helmet>
 );
 
