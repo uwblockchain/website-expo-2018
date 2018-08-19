c71e281b8934b292d6efe7c884554f5f26b03b52
Brady Pascoe <bpas247@hotmail.com>
2018-08-18 18:57:28 -0700

    Add Header (#9)
    * Modified app.js
    
    * Added logo, title, and subtitle
    
    * Restructured directory
    
    * Added navigation menu
    
    * Material UI demo
    
    * Added more components to navigation
    
    * removed top-app-bar
    
    decided not to use it
    
    * Clarified tab returns
    
    * Aligned elements
    
    * Added date
    
    * Added title
    
    * fixed styling that made one of the containers dissapear
    
    * Added prop checking
    
    * Added additional unit tests for components
    
    * Updated material-ui
    
    * Removed legacy file and test
    
    * Reorganized files
    
    * Now properly binding function
    
    * Added additional DOM test
    
    * Removed another legacy file
    
    * Added link to button
    
    * Migrated to react-bootstrap
    
    * Fixed test
    
    * Updated styling
    
    * Added responsiveness to navbar
    
    added functionality to button
    
    * Added header functionality
    
    will now switch the view of the content on the page
    
    * Added react-helmet
    
    * Updated font and modulated head
    
    * Updated styling more
    
    * centered list items in mobile
    
    * Fixed letter-spacing for title
    
    * Fixed package-lock after merge
    
    * Updated App to be stateless
    
    * Added enzyme testing
    
    * Refactored navigation tests
    
    * Added more navigation tests
    
    * Refactored Navigation
    
    * Refactored App test to use enzyme
    
    * Updated homepage to be correct URL deployment
    
    * Changed some css for resposnive behaviour and vertical alignment of item
    
    * Moved css to index.css
    
    * Wrapped Navigation tabs in its own component
    
    * Updated navigation to use NavTabs
    
    * Updated yarn.lock
    
    * removed caching
    
    * moved navigation to different folder
    
    * removed babel-core
    
    not needed
    
    * integrated all the work
    
    * removed commented out code
    
    * Modulated out navigation components
    
    * Added another navigation test
    
    * Migrated all head elements to the Head component

--- src/js/App.js
+++ src/js/App.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
 import '../css/App.css';
+import React from 'react';
+import Navigation from './nav/Navigation';
+import Head from './Head';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossOrigin="anonymous" />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <Head />
+    <Navigation />
+    <div id="content" />
+  </div>
+);
 
 export default App;
