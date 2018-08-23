de85006554db2956bdb255cd54b87a8fa6339e0c
DugganB <dugganmail@aol.com>
2018-08-23 15:24:15 -0700

    Add buy tickets (#15)
    * Initial commit with cards
    
    * Added styling for cards
    
    * Added mobile support and some more styling and structuring
    
    * Removed cursive font

---
+++ src/js/buyTickets/BuyTickets.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import BuyTicketCard from './BuyTicketCard';
+import { Row, Col } from 'react-bootstrap';
+
+class BuyTickets extends React.Component {
+
+    render() {
+        return(
+            <div className="container">
+                <Row>
+                    <Col md={4}>
+                        <BuyTicketCard className="" title="Students" price="$5" fee="$2.00"/>
+                    </Col>
+                    <Col md={4}>
+                        <BuyTicketCard title="General Admission" sub="(Early Bird)" price="$99" fee="$7.57"/>
+                    </Col>
+                    <Col md={4}>
+                        <BuyTicketCard title="General Admission" price="$149" fee="$13.15"/>
+                    </Col>
+                </Row>
+            </div>
+        )
+    }
+
+}
+
+export default BuyTickets
