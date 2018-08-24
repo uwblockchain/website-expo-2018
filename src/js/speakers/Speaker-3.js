631c7b0fa57ff044b1569da13001f0bd0b858e36
Brady Pascoe <bpas247@hotmail.com>
2018-08-23 20:10:58 -0700

    Now using JSX literal expressions
    instead of JSX variables, looks cleaner

--- src/js/speakers/Speaker-3.js
+++ src/js/speakers/Speaker-3.js
@@ -2,22 +2,29 @@ import React from 'react';
 import speakerImg from '../../img/joshua_shane.jpg';
 import Speaker from './Speaker';
 
-const job = <p>CHIEF MARKETING OFFICER, LIFEID</p>;
-
-const name = <b>Joshua Shane</b>;
-
-const body =
-  <p>
-    Over the past 25 years, Joshua has built and run marketing organizations for Silicon
-    Valley startups and global Seattle tech giants. His passion is bringing technology into
-    service for local, regional and global communities. Prior to being CMO at lifeID,
-    Joshua was Managing Director and VP of Strategy for Viewstream, a marketing
-    agency focused on “deep technology,” where he started and ran the agency’s
-    Blockchain marketing practice. 
-  </p>;
-
 const Speaker3 = () => (
-  <Speaker img={speakerImg} job={job} name={name} body={body} /> 
+  <Speaker 
+    img={speakerImg} 
+    job=
+    {
+      <p>CHIEF MARKETING OFFICER, LIFEID</p>
+    } 
+    name=
+    {
+      <b>Joshua Shane</b>
+    } 
+    body=
+    {
+      <p>
+        Over the past 25 years, Joshua has built and run marketing organizations for Silicon
+        Valley startups and global Seattle tech giants. His passion is bringing technology into
+        service for local, regional and global communities. Prior to being CMO at lifeID,
+        Joshua was Managing Director and VP of Strategy for Viewstream, a marketing
+        agency focused on “deep technology,” where he started and ran the agency’s
+        Blockchain marketing practice. 
+      </p>  
+    }
+  /> 
 );
 
 export default Speaker3;
