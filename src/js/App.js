aa35fdc9e44a25de5aaefe66b75a0750f0cefc75
DugganB <dugganmail@aol.com>
2018-08-28 14:46:48 -0700

    Added video component to mainpage

--- src/js/App.js
+++ src/js/App.js
@@ -4,11 +4,13 @@ import Navigation from './nav/Navigation';
 import Head from './Head';
 import BuyTickets from './buyTickets/BuyTickets';
 import SpeakerPreviews from './speakers/SpeakerPreviews';
+import Video from './video/Video';
 
 const App = () => (
   <div className="App">
     <Head />
     <Navigation />
+    <Video />
     <BuyTickets/>
     <SpeakerPreviews/>
     <div id="content" />
