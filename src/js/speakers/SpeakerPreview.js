13492ef7e9437b4408f1418e1ac9532e3b10e23d
Brady Pascoe <bpas247@hotmail.com>
2018-08-30 00:29:29 -0700

    Redesigned preview to use a model

--- src/js/speakers/SpeakerPreview.js
+++ src/js/speakers/SpeakerPreview.js
@@ -1,13 +1,52 @@
 import React from 'react';
-import { Image, Row, Col } from 'react-bootstrap';
+import { Image, Modal, Button } from 'react-bootstrap';
+import Speaker from './Speaker';
 
-const SpeakerPreview = (props) => (
-    <div>
-        <Image src={props.img} responsive/>
-        <h3 className="speaker-preview-name">{props.name}</h3>
-        <p className="speaker-preview-job">{props.job}</p>
-    </div>
-)
-
+class SpeakerPreview extends React.Component {
+    constructor(props, context) {
+      super(props, context);
+  
+      this.handleShow = this.handleShow.bind(this);
+      this.handleClose = this.handleClose.bind(this);
+  
+      this.state = {
+        show: false
+      };
+    }
+  
+    handleClose() {
+      this.setState({ show: false });
+    }
+  
+    handleShow() {
+      this.setState({ show: true });
+    }
+  
+    render() {  
+      return (
+        <div>  
+          <div onClick={this.handleShow}>
+            <Image src={this.props.img} responsive/>
+            <h3 className="speaker-preview-name">{this.props.name}</h3>
+            <p className="speaker-preview-job">{this.props.job}</p>
+          </div>
+  
+          <Modal show={this.state.show} onHide={this.handleClose}>
+            <Modal.Header>
+              <Button onClick={this.handleClose}>Back</Button>
+            </Modal.Header>
+            <Modal.Body>
+              <Speaker 
+                img={this.props.img}
+                job={this.props.job}
+                name={this.props.name}
+                body={this.props.body}
+              />
+            </Modal.Body>
+          </Modal>
+        </div>
+      );
+    }
+  }
 
 export default SpeakerPreview;
