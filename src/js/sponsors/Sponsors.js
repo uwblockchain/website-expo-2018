67a9696475d396b1f5a3ce058f7c3d19cf34766d
Brady Pascoe <bpas247@hotmail.com>
2018-08-31 18:43:44 -0700

    Added rough draft of sponsors component

--- src/js/sponsors/Sponsors.js
+++ src/js/sponsors/Sponsors.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Grid, Row, Button, Image } from 'react-bootstrap';
+import comotionImg from '../../img/sponsors/comotion-logo-white.png';
+import galvanizeImg from '../../img/sponsors/galvanize-logo.png';
 
 const Sponsors = () => (
-    <div name="sponsors">
+  <div name="sponsors">
+    <Grid>
+      <Row>
+        <h2>Sponsors</h2>
+        <h4>This exposition would not be possible without help from these great sponsors.</h4>
+      </Row>
+      <Row md={2}>
+        <Image src={galvanizeImg} />
+        <Image src={comotionImg}/>
+      </Row>
+      <Row>
+        <h4>
+          If your company is interested in supporting this
+          event, contact us at info@blockchainsocietyuw.com.
+        </h4>
+      </Row>
+      <Row>
+        <Button href="mailto:info@blockchainsocietyuw.com">LEARN MORE</Button>
+      </Row>
+    </Grid>
+  </div>
+);
 
-    </div>
-)
-
-export default Sponsors
+export default Sponsors;
