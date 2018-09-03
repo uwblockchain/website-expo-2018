780be5f5c2a0305e29368cb73c26bfec088a6b08
DugganB <dugganmail@aol.com>
2018-09-02 20:29:06 -0700

    Added styling, ready for merge

--- src/js/sponsors/Sponsors.js
+++ src/js/sponsors/Sponsors.js
@@ -1,27 +1,31 @@
 import React from 'react';
-import { Grid, Row, Button, Image } from 'react-bootstrap';
+import { Grid, Row, Button, Image, Col } from 'react-bootstrap';
 import comotionImg from '../../img/sponsors/comotion-logo-white.png';
 import galvanizeImg from '../../img/sponsors/galvanize-logo.png';
 
 const Sponsors = () => (
-  <div name="sponsors">
+  <div name="sponsors" className="sponsors">
     <Grid>
       <Row>
-        <h2>Sponsors</h2>
-        <h4>This exposition would not be possible without help from these great sponsors.</h4>
+        <h2 className="sponsors-title">Sponsors</h2>
+        <p className="sponsors-text">This exposition would not be possible without<br/>help from these great sponsors.</p>
       </Row>
-      <Row md={2}>
-        <Image src={galvanizeImg} />
-        <Image src={comotionImg}/>
-      </Row>
-      <Row>
-        <h4>
-          If your company is interested in supporting this
-          event, contact us at info@blockchainsocietyuw.com.
-        </h4>
+      <Row >
+        <Col md={6}>
+          <Image className="sponsors-logo" src={galvanizeImg} />
+        </Col>
+        <Col md={6}>
+          <Image className="sponsors-logo coMotion-logo" src={comotionImg}/>
+        </Col>
       </Row>
       <Row>
-        <Button href="mailto:info@blockchainsocietyuw.com">LEARN MORE</Button>
+        <p className="sponsors-text">
+          If your company is interested in supporting this<br/>
+          event, contact us at <a href="mailto:info@blockchainsocietyuw.com">info@blockchainsocietyuw.com</a>.
+        </p>
+      </Row>
+      <Row>
+        <a className="btn btn-default" href="mailto:info@blockchainsocietyuw.com">LEARN MORE</a>
       </Row>
     </Grid>
   </div>
