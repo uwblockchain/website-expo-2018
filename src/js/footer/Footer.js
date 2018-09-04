a38cc5c289640166741da839f8c8f87b7a4bcac7
DugganB <dugganmail@aol.com>
2018-09-03 17:36:22 -0700

    Updated footer styling

--- src/js/footer/Footer.js
+++ src/js/footer/Footer.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { NavButton } from '../nav/Navigation';
+import { Row, Col } from 'react-bootstrap';
 
 const Footer = () => (
-  <div className="footer" style={{background: 'black'}}>
-    <p style={{color: 'white'}}>
-      Copyright © 2018 Blockchain Society at the UW
-      <br />
-      info@blockchainsocietyuw.com
-    </p>
-    <NavButton />
+  <div className="container-flex footer">
+    <div className="container">
+      <Row className="">
+        <Col md={4} mdOffset={2}>
+          <p style={{color: 'white'}}>
+            Copyright © 2018 Blockchain Society at the UW
+            <br />
+            info@blockchainsocietyuw.com
+          </p>
+        </Col>
+        <Col md={4} style={{textAlign: 'right', paddingTop: '0.05em'}}>
+          <NavButton />
+        </Col>
+      </Row>  
+    </div>
   </div>
 );
 
