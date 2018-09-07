26ecdec7be6f7da13b85f4818fed89337c63c9c6
Brady Pascoe <bpas247@hotmail.com>
2018-09-07 01:50:19 -0700

    Changed to stateless component

--- src/js/nav/NavTabs.js
+++ src/js/nav/NavTabs.js
@@ -2,35 +2,32 @@ import React from 'react';
 import { NavItem, Nav } from 'react-bootstrap';
 import { scroller } from 'react-scroll';
 
-export default class NavTabs extends React.Component {
-
-  scrollTo(elementToScroll) {
-    scroller.scrollTo(elementToScroll, {
-      duration: 800,
-      delay: 0,
-      smooth: 'easeInOutQuart'
-    });
-  };
-
-  render() {
-    return(
-      <Nav className="nav-tabs">
-        <NavItem id="nav-text" eventKey={1} onClick={() => this.scrollTo('speaker-previews')}>
-          Speakers
-        </NavItem>
-        <NavItem id="nav-text" eventKey={2} onClick={() => this.scrollTo('schedule')}>
-          Schedule
-        </NavItem>
-        <NavItem id="nav-text" eventKey={3} onClick={() => this.scrollTo('workshops')}>
-          Workshops
-        </NavItem>
-        <NavItem id="nav-text" eventKey={4} onClick={() => this.scrollTo('sponsors')}>
-          Sponsors
-        </NavItem>
-        <NavItem id="nav-text" eventKey={5} onClick={() => this.scrollTo('contact')}>
-          Contact
-        </NavItem>
-      </Nav>
-    );
-  }
+function scrollTo(elementToScroll) {
+  scroller.scrollTo(elementToScroll, {
+    duration: 800,
+    delay: 0,
+    smooth: 'easeInOutQuart'
+  });
 };
+
+const NavTabs = () => (
+  <Nav className="nav-tabs">
+    <NavItem id="nav-text" eventKey={1} onClick={() => scrollTo('speaker-previews')}>
+      Speakers
+    </NavItem>
+    <NavItem id="nav-text" eventKey={2} onClick={() => scrollTo('schedule')}>
+      Schedule
+    </NavItem>
+    <NavItem id="nav-text" eventKey={3} onClick={() => scrollTo('workshops')}>
+      Workshops
+    </NavItem>
+    <NavItem id="nav-text" eventKey={4} onClick={() => scrollTo('sponsors')}>
+      Sponsors
+    </NavItem>
+    <NavItem id="nav-text" eventKey={5} onClick={() => scrollTo('contact')}>
+      Contact
+    </NavItem>
+  </Nav>
+);
+
+export default NavTabs;
