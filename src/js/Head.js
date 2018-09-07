fcf99b81ef8a06d2e6f78cfbc1e515bed1250c55
DugganB <dugganmail@aol.com>
2018-09-06 18:56:09 -0700

    Updated favicons again and the headers to block localhost on analytics

--- src/js/Head.js
+++ src/js/Head.js
@@ -4,26 +4,17 @@ import Helmet from 'react-helmet';
 const Head = () => (
   <Helmet>
     {/* FAVICONS */}
-    <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />>
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />>
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
-    <link rel="manifest" href="/favicons/site.webmanifest" />
-    <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#6938bf" />
-    <link rel="shortcut icon" href="/favicons/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="uwbce.com/favicons/apple-touch-icon.png" />>
+    <link rel="icon" type="image/png" sizes="32x32" href="uwbce.com/favicons/favicon-32x32.png" />>
+    <link rel="icon" type="image/png" sizes="16x16" href="uwbce.com/favicons/favicon-16x16.png" />
+    <link rel="manifest" href="uwbce.com/favicons/site.webmanifest" />
+    <link rel="mask-icon" href="uwbce.com/favicons/safari-pinned-tab.svg" color="#6938bf" />
+    <link rel="shortcut icon" href="uwbce.com/favicons/favicon.ico" />
     <meta name="msapplication-TileColor" content="#2b5797" />
-    <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
+    <meta name="msapplication-config" content="uwbce.com/favicons/browserconfig.xml" />
     <meta name="theme-color" content="#ffffff" />
 
-    {/* ANALYTICS */}
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-125422568-1"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments)}
-      gtag('js', new Date());
-
-      gtag('config', 'UA-125422568-1');
-    </script>
-
+    
     {/* STYLE SHEETS */}
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
