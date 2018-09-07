38b505af9b55c29f409ae4c9612102389df54387
DugganB <dugganmail@aol.com>
2018-09-06 18:23:20 -0700

    Added favicons and updated head.js with favicons and analytics

--- src/js/Head.js
+++ src/js/Head.js
@@ -3,6 +3,28 @@ import Helmet from 'react-helmet';
 
 const Head = () => (
   <Helmet>
+    {/* FAVICONS */}
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />>
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />>
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+    <link rel="manifest" href="/favicons/site.webmanifest" />
+    <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#6938bf" />
+    <link rel="shortcut icon" href="/favicons/favicon.ico" />
+    <meta name="msapplication-TileColor" content="#2b5797" />
+    <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
+    <meta name="theme-color" content="#ffffff" />
+
+    {/* ANALYTICS */}
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-125422568-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments)}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-125422568-1');
+    </script>
+
+    {/* STYLE SHEETS */}
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossOrigin="anonymous" />
