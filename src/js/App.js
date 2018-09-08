08354dd6dc7f65dadccedfc05e6ad7cfa90d6fc8
Brady Pascoe <bpas247@hotmail.com>
2018-09-07 19:28:31 -0700

    More prettier formatting

--- src/js/App.js
+++ src/js/App.js
@@ -14,35 +14,35 @@ import { Element } from 'react-scroll';
 import loadicon from '../img/loadicon.gif';
 import LoadingScreen from 'react-loading-screen';
 
-export default class App extends React.Component { 
+export default class App extends React.Component {
   // Decided to keep the delay but keep it
   // a small value, since the loading screen
   // will sometimes transition before all of
   // the components are rendered properly
   delay() {
-    return new Promise(resolve => setTimeout(resolve, 75))
+    return new Promise(resolve => setTimeout(resolve, 75));
   }
 
   constructor() {
     super();
-    this.state={ loading: true }
-  } 
+    this.state = { loading: true };
+  }
 
   componentDidMount() {
     this.delay().then(() => {
-      this.setState({ loading: false })
+      this.setState({ loading: false });
     });
   }
 
   render() {
     return (
       <LoadingScreen
-      loading={this.state.loading}
-      bgColor='black'
-      textColor='white'
-      logoSrc={loadicon}
-      text='Loading...'
-      > 
+        loading={this.state.loading}
+        bgColor="black"
+        textColor="white"
+        logoSrc={loadicon}
+        text="Loading..."
+      >
         <div className="App">
           <Head />
           <Navigation />
@@ -64,6 +64,6 @@ export default class App extends React.Component {
           <Footer />
         </div>
       </LoadingScreen>
-      );
-    }
+    );
   }
+}
