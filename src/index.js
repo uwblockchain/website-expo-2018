c7003cc69d40416a0da14fbf91865f0dd55846a5
Brady Pascoe <bpas247@hotmail.com>
2018-09-22 16:08:44 -0700

    Reverted commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import './css/index.css';
 import React from 'react';
-import { hydrate, render } from 'react-dom';
+import { render } from 'react-dom';
 
 import App from './js/App';
 import registerServiceWorker from './js/registerServiceWorker';
 
-const rootElement = document.getElementById('root');
-if (rootElement.hasChildNodes()) {
-  hydrate(<App />, rootElement);
-} else {
-  render(<App />, rootElement);
-}
+render(<App />, document.getElementById('root'));
 registerServiceWorker();
