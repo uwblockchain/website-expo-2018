82182c0cd4b15f95c9b3a1ee0cf24db0d88949e7
Brady Pascoe <bpas247@hotmail.com>
2018-09-22 15:59:12 -0700

    Integrated react-snap

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import './css/index.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { hydrate, render } from 'react-dom';
 
 import App from './js/App';
 import registerServiceWorker from './js/registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
 registerServiceWorker();
